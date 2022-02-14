71dfd8f1fd163666d4fc44442da0253be5ab46e2
ifotn <rich@infrontofthenet.com>
2022-02-14 08:52:02 -0500

    l6/1 - start delete

--- movies.php
+++ movies.php
@@ -6,6 +6,7 @@
         <!-- Bootstrap CDN -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script src="js/scripts.js" type="text/javascript" defer></script>
     </head>
     <body>
         <h1>Movies</h1>
@@ -17,6 +18,7 @@
                     <th>Rating</th>
                     <th>Release Year</th>
                     <th>Genre</th>
+                    <th>Actions</th>
                 </tr>
            </thead>                
            <tbody>
@@ -35,9 +37,15 @@
                         <td>' . $movie['rating'] . '</td>
                         <td>' . $movie['releaseYear'] . '</td>
                         <td>' . $movie['name'] . '</td>
+                        <td>
+                            <a class="btn btn-danger" 
+                                onclick="return confirmDelete()"
+                                href="delete-movie.php?movieId=' . $movie['movieId'] . '">
+                                Delete
+                            </a>
+                        </td>
                         </tr>';
                 }
-
                 $db = null;
                 ?>
            </tbody> 
